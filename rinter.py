86afc603350b0df49d1192d81417031bf8eed352
bhunao <bhunao@gmail.com>
2023-04-08 12:13:56 -0300

    created: dirnter a way to print dir stuff

--- rinter.py
+++ rinter.py
@@ -10,7 +10,22 @@ class rinter:
         self._v = content
         print(*content)
 
+def dirnter(obj, sep=' | ', end='\n'):
+    for option in dir(obj):
+        text = option
+        if callable(getattr(obj, option)):
+            text += "()"
+        print(text)
+
+    # print(*dirs, sep=sep, end=end)
+
 rinter() >> 3141592     # `>>` prints 
 rinter = rinter()       # `>>` prints 
 rinter >> "text", 3, 5  # for some reasong the first element is None
 rinter << 5             # `<<`store the value inside rinter and prints it
+rinter >> ""
+
+
+
+tst = [1,2,3]
+dirnter(tst)
